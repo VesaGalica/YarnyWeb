ff47dcc890ad3208d00cad0b915a9ccdd41476f9
Vesa <vesagalica@gmail.com>
2023-10-01 22:51:30 +0200

    create header component

--- src/app/components/home-page/home-page.component.ts
+++ src/app/components/home-page/home-page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {HeaderComponent} from "../../shared/header/header.component";
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, HeaderComponent],
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.scss']
 })
